e87a74386e4634c339c5e3a0617bea5a18fed5f9
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-04-16 17:57:46 +0200

    Add initial DCA for tl_course

---
+++ src/Resources/contao/dca/tl_course.php
@@ -0,0 +1,95 @@
+<?php
+
+use Contao\DC_Table;
+
+$GLOBALS['TL_DCA']['tl_course'] = [
+    'config' => [
+        'dataContainer' => DC_Table::class,
+        'enableVersioning' => true,
+        'sql' => [
+            'keys' => [
+                'id' => 'primary',
+            ],
+        ],
+    ],
+
+    'list' => [
+        'sorting' => [
+            'mode' => 1,
+            'fields' => ['sorting'],
+            'flag' => 1,
+            'panelLayout' => 'sort,filter;search,limit',
+        ],
+        'label' => [
+            'fields' => ['name'],
+            'format' => '%s',
+        ],
+        'global_operations' => [
+            'all' => [
+                'href' => 'act=select',
+                'class' => 'header_edit_all',
+                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"',
+            ],
+        ],
+        'operations' => [
+            'edit' => [
+                'href' => 'act=edit',
+                'icon' => 'edit.svg',
+            ],
+            'copy' => [
+                'href' => 'act=paste&amp;mode=copy',
+                'icon' => 'copy.svg',
+            ],
+            'delete' => [
+                'href' => 'act=delete',
+                'icon' => 'delete.svg',
+                'attributes' => 'onclick="if(!confirm(\'Are you sure you want to delete this entry?\'))return false;Backend.getScrollOffset()"',
+            ],
+            'show' => [
+                'href' => 'act=show',
+                'icon' => 'show.svg',
+            ],
+        ],
+    ],
+
+    'palettes' => [
+        'default' => '{title_legend},name,form_reference;{publish_legend},published',
+    ],
+
+    'fields' => [
+        'id' => [
+            'sql' => "int(10) unsigned NOT NULL auto_increment",
+        ],
+        'tstamp' => [
+            'sql' => "int(10) unsigned NOT NULL default 0",
+        ],
+        'sorting' => [
+            'sql' => "int(10) unsigned NOT NULL default 0",
+        ],
+        'name' => [
+            'inputType' => 'text',
+            'eval' => [
+                'mandatory' => true,
+                'maxlength' => 255,
+                'tl_class' => 'w50',
+            ],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
+        'form_reference' => [
+            'inputType' => 'text',
+            'eval' => [
+                'mandatory' => true,
+                'maxlength' => 255,
+                'tl_class' => 'w50',
+            ],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
+        'published' => [
+            'inputType' => 'checkbox',
+            'eval' => [
+                'tl_class' => 'w50 m12',
+            ],
+            'sql' => "char(1) NOT NULL default ''",
+        ],
+    ],
+];
