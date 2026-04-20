3941bddae9e664e94a9f9fa0418fe65aa0d36293
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-04-20 15:52:31 +0200

    refactor: align english labels with updated course date fields

--- src/Resources/contao/languages/en/tl_course_date.php
+++ src/Resources/contao/languages/en/tl_course_date.php
@@ -2,11 +2,16 @@
 
 $GLOBALS['TL_LANG']['tl_course_date']['start_date'] = ['Start date', 'Enter the start date.'];
 $GLOBALS['TL_LANG']['tl_course_date']['end_date'] = ['End date', 'Enter the end date.'];
-$GLOBALS['TL_LANG']['tl_course_date']['add_time'] = ['Add Timen'];
+$GLOBALS['TL_LANG']['tl_course_date']['add_time'] = ['Add time', 'Enable this option if a time should be specified for the course date.'];
 $GLOBALS['TL_LANG']['tl_course_date']['start_time'] = ['Start time', 'Enter the start time.'];
 $GLOBALS['TL_LANG']['tl_course_date']['end_time'] = ['End time', 'Enter the end time.'];
-$GLOBALS['TL_LANG']['tl_course_date']['location'] = ['Location', 'Enter the course date location.'];
-$GLOBALS['TL_LANG']['tl_course_date']['fully_booked'] = ['Fully booked', 'Mark if this date is fully booked.'];
+
+$GLOBALS['TL_LANG']['tl_course_date']['venue'] = ['Location', 'Enter the location or city of the course date.'];
+$GLOBALS['TL_LANG']['tl_course_date']['postal_code'] = ['Postal code', 'Enter the postal code.'];
+$GLOBALS['TL_LANG']['tl_course_date']['street'] = ['Street', 'Enter the street.'];
+$GLOBALS['TL_LANG']['tl_course_date']['house_number'] = ['House number', 'Enter the house number.'];
+
+$GLOBALS['TL_LANG']['tl_course_date']['fully_booked'] = ['Fully booked', 'Mark this option if the course date is fully booked.'];
 $GLOBALS['TL_LANG']['tl_course_date']['published'] = ['Published', 'Make the date visible in the frontend.'];
 
 $GLOBALS['TL_LANG']['tl_course_date']['date_legend'] = 'Date information';
