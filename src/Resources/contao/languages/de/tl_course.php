a36d57042d81dff4be18324be8da461fc525ff11
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-04-20 15:53:24 +0200

    refactor: clean up german course language labels

--- src/Resources/contao/languages/de/tl_course.php
+++ src/Resources/contao/languages/de/tl_course.php
@@ -1,15 +1,14 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_course']['title'] = ['Titel', 'Geben Sie den Namen des Kurses ein.'];
-$GLOBALS['TL_LANG']['tl_course']['form_reference'] = ['Formular', 'Wählen Sie das zugehörige Formular aus.'];
-$GLOBALS['TL_LANG']['tl_course']['published'] = ['Veröffentlicht', 'Den Kurs im Frontend anzeigen.'];
-$GLOBALS['TL_LANG']['tl_course']['course_dates'] = ['Kurstermine', 'Die Kurstermine dieses Kurses verwalten'];
+$GLOBALS['TL_LANG']['tl_course']['title'] = ['Titel', 'Geben Sie den Titel des Kurses ein.'];
 $GLOBALS['TL_LANG']['tl_course']['author'] = ['Autor', 'Geben Sie den Autor ein.'];
 $GLOBALS['TL_LANG']['tl_course']['description'] = ['Beschreibung', 'Geben Sie eine Beschreibung des Kurses ein.'];
 $GLOBALS['TL_LANG']['tl_course']['preview_image'] = ['Vorschaubild', 'Wählen Sie ein Vorschaubild aus.'];
+$GLOBALS['TL_LANG']['tl_course']['form_reference'] = ['Formular', 'Wählen Sie das zugehörige Formular aus.'];
+$GLOBALS['TL_LANG']['tl_course']['published'] = ['Veröffentlicht', 'Den Kurs im Frontend anzeigen.'];
 
-$GLOBALS['TL_LANG']['tl_course']['title_legend'] = 'Kursdaten';
-$GLOBALS['TL_LANG']['tl_course']['publish_legend'] = 'Veröffentlichung';
+$GLOBALS['TL_LANG']['tl_course']['course_dates'] = ['Kurstermine', 'Die Kurstermine dieses Kurses verwalten'];
+
 $GLOBALS['TL_LANG']['tl_course']['general_legend'] = 'Allgemein';
 $GLOBALS['TL_LANG']['tl_course']['description_legend'] = 'Beschreibung';
 $GLOBALS['TL_LANG']['tl_course']['media_legend'] = 'Medien und Formular';
