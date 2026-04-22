2cf786d64cd9883b5ff4939920257e1988c381b9
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-04-22 15:02:43 +0200

    refactor: align language labels with new frontend structure

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -4,14 +4,18 @@ $GLOBALS['TL_LANG']['MOD']['courses'] = ['Kurse', 'Verwaltung der Kurse'];
 
 $GLOBALS['TL_LANG']['FMD']['courses'] = 'Kurse';
 $GLOBALS['TL_LANG']['FMD']['course_list'] = ['Kursliste', 'Gibt eine Liste der Kurse aus.'];
-$GLOBALS['TL_LANG']['FMD']['course_reader'] = ['Kursleser', 'Gibt die Detailansicht eines Kurses aus.'];
+$GLOBALS['TL_LANG']['FMD']['course_reader'] = ['Kursdetails', 'Gibt die Detailansicht eines Kurses aus.'];
 
 $GLOBALS['TL_LANG']['tl_module']['course_order'] = ['Sortierung', 'Wählen Sie die Sortierreihenfolge der Kurse.'];
 $GLOBALS['TL_LANG']['tl_module']['course_order_options']['asc'] = 'Aufsteigend';
 $GLOBALS['TL_LANG']['tl_module']['course_order_options']['desc'] = 'Absteigend';
+$GLOBALS['TL_LANG']['tl_module']['registration_jumpTo'] = ['Anmeldeseite', 'Wählen Sie die Seite aus, auf der das Anmeldeformular angezeigt werden soll.'];
 
-$GLOBALS['TL_LANG']['MSC']['course_list_empty'] = 'Aktuell sind keine Kurse mit zukünftigen Terminen verfügbar.';
+$GLOBALS['TL_LANG']['MSC']['course_list_empty'] = 'Aktuell sind keine Kurse verfügbar.';
 $GLOBALS['TL_LANG']['MSC']['course_list_author'] = 'Autor';
+$GLOBALS['TL_LANG']['MSC']['course_list_details'] = 'Details anzeigen';
+$GLOBALS['TL_LANG']['MSC']['course_list_next_date'] = 'Nächster Termin';
+
 $GLOBALS['TL_LANG']['MSC']['course_list_dates'] = 'Kurstermine';
 $GLOBALS['TL_LANG']['MSC']['course_list_date'] = 'Datum';
 $GLOBALS['TL_LANG']['MSC']['course_list_time'] = 'Zeit';
@@ -19,5 +23,3 @@ $GLOBALS['TL_LANG']['MSC']['course_list_location'] = 'Ort';
 $GLOBALS['TL_LANG']['MSC']['course_list_status'] = 'Status';
 $GLOBALS['TL_LANG']['MSC']['course_list_fully_booked'] = 'Ausgebucht';
 $GLOBALS['TL_LANG']['MSC']['course_list_available'] = 'Verfügbar';
-
-$GLOBALS['TL_LANG']['tl_module']['registration_jumpTo'] = ['Anmeldeseite', 'Wählen Sie die Seite aus, auf der das Anmeldeformular angezeigt werden soll.'];
--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -3,15 +3,19 @@
 $GLOBALS['TL_LANG']['MOD']['courses'] = ['Courses', 'Manage courses'];
 
 $GLOBALS['TL_LANG']['FMD']['courses'] = 'Courses';
-$GLOBALS['TL_LANG']['FMD']['course_list'] = ['Course list', 'Outputs a list of courses.'];
-$GLOBALS['TL_LANG']['FMD']['course_reader'] = ['Course reader', 'Outputs the detail view of a course.'];
+$GLOBALS['TL_LANG']['FMD']['course_list'] = ['Course list', 'Displays a list of courses.'];
+$GLOBALS['TL_LANG']['FMD']['course_reader'] = ['Course details', 'Displays the detail view of a course.'];
 
 $GLOBALS['TL_LANG']['tl_module']['course_order'] = ['Sorting', 'Select the sorting order of the courses.'];
 $GLOBALS['TL_LANG']['tl_module']['course_order_options']['asc'] = 'Ascending';
 $GLOBALS['TL_LANG']['tl_module']['course_order_options']['desc'] = 'Descending';
+$GLOBALS['TL_LANG']['tl_module']['registration_jumpTo'] = ['Registration page', 'Select the page where the registration form will be displayed.'];
 
-$GLOBALS['TL_LANG']['MSC']['course_list_empty'] = 'There are currently no courses with upcoming dates available.';
+$GLOBALS['TL_LANG']['MSC']['course_list_empty'] = 'There are currently no courses available.';
 $GLOBALS['TL_LANG']['MSC']['course_list_author'] = 'Author';
+$GLOBALS['TL_LANG']['MSC']['course_list_details'] = 'View details';
+$GLOBALS['TL_LANG']['MSC']['course_list_next_date'] = 'Next date';
+
 $GLOBALS['TL_LANG']['MSC']['course_list_dates'] = 'Course dates';
 $GLOBALS['TL_LANG']['MSC']['course_list_date'] = 'Date';
 $GLOBALS['TL_LANG']['MSC']['course_list_time'] = 'Time';
@@ -19,5 +23,3 @@ $GLOBALS['TL_LANG']['MSC']['course_list_location'] = 'Location';
 $GLOBALS['TL_LANG']['MSC']['course_list_status'] = 'Status';
 $GLOBALS['TL_LANG']['MSC']['course_list_fully_booked'] = 'Fully booked';
 $GLOBALS['TL_LANG']['MSC']['course_list_available'] = 'Available';
-
-$GLOBALS['TL_LANG']['tl_module']['registration_jumpTo'] = ['Anmeldeseite', 'Wählen Sie die Seite aus, auf der das Anmeldeformular angezeigt werden soll.'];
