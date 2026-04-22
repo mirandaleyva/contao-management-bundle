9ea432b89feee3e2a8a8027ac204c32e7c97c498
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-04-22 12:24:53 +0200

    Fix: FE_MOD from config.php delete

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,7 +4,7 @@ $GLOBALS['BE_MOD']['content']['courses'] = [
   'tables' => ['tl_course', 'tl_course_date'],
 ];
 
-$GLOBALS['FE_MOD']['courses'] = [
-  'course_list' => MirandaLeyva\ContaoCourseManagementBundle\Controller\FrontendModule\CourseListController::class,
-  'course_reader' => MirandaLeyva\ContaoCourseManagementBundle\Controller\FrontendModule\CourseReaderController::class,
-];
+// $GLOBALS['FE_MOD']['courses'] = [
+//   'course_list' => MirandaLeyva\ContaoCourseManagementBundle\Controller\FrontendModule\CourseListController::class,
+//   'course_reader' => MirandaLeyva\ContaoCourseManagementBundle\Controller\FrontendModule\CourseReaderController::class,
+// ];
